ab840c059ebf0b754d72cab47eef3220ae215d49
Marcelo Camera <mcamera.dev@pm.me>
2025-06-14 15:04:37 -0300

    add database connection on departments endpoint

--- api/app/main.py
+++ api/app/main.py
@@ -1,7 +1,8 @@
 from typing import Optional
 
+from db import get_mysql_connection
 from fastapi import FastAPI, File, HTTPException, UploadFile, status
-
+from mysql.connector import ProgrammingError, errorcode
 from utils import get_logger
 
 logger = get_logger(__name__)
@@ -40,19 +41,61 @@ async def departments(csv_file: Optional[UploadFile] = File(None)) -> dict:
             detail="File is empty! Send at least 1 record.",
         )
 
+    # connecting to MySQL database
+    db_conn = get_mysql_connection()
+    cursor = db_conn.cursor()
+
+    try:
+        cursor.execute("SELECT * FROM departments;")
+        results = cursor.fetchall()
+        cursor.close()
+        db_conn.close()
+
+        if not results:
+            logger.warning("No departments found in the table.")
+            return {"message": "No departments found."}
+
+        return [{"id": r[0], "department": r[1]} for r in results]
+
+    except ProgrammingError as err:
+        if err.errno == errorcode.ER_BAD_TABLE_ERROR:
+            logger.error("Table does not exist!")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Table does not exist!",
+            )
+        else:
+            logger.error(f"MySQL Error: {err}")
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(err)
+            )
+
+    except Exception as e:
+        logger.error(f"Unexpected error: {e}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+        )
+
 
 @app.post("/jobs")
 async def jobs() -> dict:
     return {"message": "jobs endpoint for send jobs data."}
 
+
 @app.post("/employees")
 async def employees() -> dict:
     return {"message": "Employees endpoint for send employees data."}
 
+
 @app.get("/quantity_employees_hired_by_quarters/{year}")
 async def quantity_employees_hired_by_quarters(year: int) -> dict:
-    return {"message": f"Shows the quantity of employees hired by quarters from the year {year}"}
+    return {
+        "message": f"Shows the quantity of employees hired by quarters from the year {year}"
+    }
+
 
 @app.get("/quantity_employees_hired_more_than_year_mean_by_department/{year}")
 async def quantity_employees_hired_more_than_year_mean_by_department(year: int) -> dict:
-    return {"message": f"Shows the quantity of employees hired more than year mean, by department, from the year {year}"}
+    return {
+        "message": f"Shows the quantity of employees hired more than year mean, by department, from the year {year}"
+    }
