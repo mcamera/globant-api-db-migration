03f4cb5a2512fc4e0081c8b84b18cfa05216a1c1
Marcelo Camera <mcamera.dev@pm.me>
2025-06-14 16:08:01 -0300

    modularize db function validations to be reused for the others endpoints

--- api/app/db.py
+++ api/app/db.py
@@ -1,34 +1,65 @@
 import os
 
 import mysql.connector
-from mysql.connector import errorcode
-
+from fastapi import HTTPException, status
+from mysql.connector import ProgrammingError, errorcode
 from utils import get_logger
 
 logger = get_logger(__name__)
 
 
 def get_mysql_connection():
-
+    """Creates a connection to the MySQL database."""
     try:
         logger.info("Connecting to MySQL database...")
         db_conn = mysql.connector.connect(
             user=os.getenv("MYSQL_USER"),
             password=os.getenv("MYSQL_PASSWORD"),
-            host="mysql",
+            host=os.getenv("MYSQL_HOST", "mysql"),
             database="database",
         )
 
         logger.info("MySQL database connected successfully.")
+
         return db_conn
 
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            logger.error("User name or password incorrect!")
-            return err
+            logger.error("Username or password incorrect!")
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Username or password incorrect!",
+            )
+
         elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            logger.error("Database does not exist!")
-            return {**err, "message": "Database does not exist!"}
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                detail="Username or password incorrect!",
+            )
         else:
             logger.error(err)
-            return err
+            raise HTTPException(
+                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(err)
+            )
+
+
+def handle_db_errors(err: ProgrammingError):
+    """Handle database errors and raise appropriate HTTP exceptions.
+
+    Args:
+        err (ProgrammingError): The error raised by the database.
+
+    Raises:
+        HTTPException: Raises an HTTPException with a status code and detail message based on the error.
+    """
+    if err.errno == errorcode.ER_BAD_TABLE_ERROR:
+        logger.error("Table does not exist!")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Table does not exist!",
+        )
+    else:
+        logger.error(f"MySQL Error: {err}")
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(err)
+        )
--- api/app/main.py
+++ api/app/main.py
@@ -1,9 +1,9 @@
 from typing import Optional
 
-from db import get_mysql_connection
+from db import get_mysql_connection, handle_db_errors
 from fastapi import FastAPI, File, HTTPException, UploadFile, status
-from mysql.connector import ProgrammingError, errorcode
-from utils import get_logger
+from mysql.connector import ProgrammingError
+from utils import get_logger, validate_upload_file
 
 logger = get_logger(__name__)
 app = FastAPI()
@@ -11,70 +11,32 @@ app = FastAPI()
 
 @app.post("/departments")
 async def departments(csv_file: Optional[UploadFile] = File(None)) -> dict:
+    validate_upload_file(csv_file, max_lines=1000)
 
-    if not csv_file:
-        logger.error("No file sent!")
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST, detail="No file sent!"
-        )
-
-    if not csv_file.filename.endswith(".csv"):
-        logger.error("File must be a CSV!")
-        raise HTTPException(
-            status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
-            detail="File must be a CSV!",
-        )
-
-    contents = csv_file.file.read()
-    contents = contents.decode("utf-8").splitlines()
-
-    if len(contents) > 1000:
-        logger.error("Too many records! Must be less than 1000 lines.")
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail="Too many records! Must be less than 1000 lines.",
-        )
-    elif len(contents) == 0:
-        logger.error("File is empty! Send at least 1 record.")
-        raise HTTPException(
-            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail="File is empty! Send at least 1 record.",
-        )
-
-    # connecting to MySQL database
     db_conn = get_mysql_connection()
     cursor = db_conn.cursor()
 
     try:
         cursor.execute("SELECT * FROM departments;")
         results = cursor.fetchall()
-        cursor.close()
-        db_conn.close()
 
         if not results:
             logger.warning("No departments found in the table.")
             return {"message": "No departments found."}
 
-        return [{"id": r[0], "department": r[1]} for r in results]
-
     except ProgrammingError as err:
-        if err.errno == errorcode.ER_BAD_TABLE_ERROR:
-            logger.error("Table does not exist!")
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                detail="Table does not exist!",
-            )
-        else:
-            logger.error(f"MySQL Error: {err}")
-            raise HTTPException(
-                status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(err)
-            )
+        handle_db_errors(err)
 
     except Exception as e:
         logger.error(f"Unexpected error: {e}")
         raise HTTPException(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e)
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail="Internal server error",
         )
+
+    finally:
+        cursor.close()
+        db_conn.close()
 
 
 @app.post("/jobs")
